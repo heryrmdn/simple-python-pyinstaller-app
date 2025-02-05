c70bef7c469684fb7a5348fb6246833b755acb9a
heryrmdn <heryrhamadan@gmail.com>
2025-02-05 17:00:02 +0700

    change docker setup file

--- docker-stop.sh
+++ docker-stop.sh
@@ -2,6 +2,8 @@ docker stop jenkins-docker
 docker stop jenkins-blueocean
 docker stop prometheus
 docker stop grafana
-: 'docker rm jenkins-docker'
-: 'docker rm jenkins-blueocean'
-: 'docker network rm jenkins'
+docker rm jenkins-docker
+docker rm jenkins-blueocean
+docker rm prometheus
+docker rm grafana
+docker network rm jenkins
